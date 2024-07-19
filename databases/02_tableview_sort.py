c2848c2fff8f53c20d230344d1f131975acb3ed3
eike-zink <user@eike-zink.de>
2024-07-19 19:29:29 +0200

    Create 02_tableview_sort.py

---
+++ databases/02_tableview_sort.py
@@ -0,0 +1,38 @@
+import sys
+
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtSql import QSqlDatabase, QSqlTableModel
+from PySide6.QtWidgets import QApplication, QMainWindow, QTableView
+
+
+class MainWindow(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.db = QSqlDatabase('QSQLITE')
+        self.db.setDatabaseName('chinook.sqlite')
+        self.db.open()
+
+        self.table = QTableView()
+        self.model = QSqlTableModel(db=self.db)
+        self.table.setModel(self.model)
+
+        self.model.setTable("Track")
+        self.model.setHeaderData(1, Qt.Orientation.Horizontal, 'Name')
+        self.model.setHeaderData(2, Qt.Orientation.Horizontal, 'Album (ID)')
+        self.model.setHeaderData(3, Qt.Orientation.Horizontal, 'Media Type (ID)')
+        self.model.setHeaderData(4, Qt.Orientation.Horizontal, 'Genre (ID)')
+        self.model.setHeaderData(5, Qt.Orientation.Horizontal, 'Composer')
+        # self.model.setSort(2, Qt.SortOrder.DescendingOrder)
+        idx = self.model.fieldIndex('Milliseconds')
+        self.model.setSort(idx, Qt.SortOrder.DescendingOrder)
+        self.model.select()
+
+        self.setMinimumSize(QSize(1024, 600))
+        self.setCentralWidget(self.table)
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
