82d522cf043db158acfd7a2e6b2939e9fd1d151a
eike-zink <user@eike-zink.de>
2024-07-22 18:49:12 +0200

    Create 01_threads.py

---
+++ threads/01_threads.py
@@ -0,0 +1,45 @@
+import sys
+import time
+
+from PySide6.QtCore import QTimer
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QPushButton, QVBoxLayout
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.counter = 0
+
+        layout = QVBoxLayout()
+
+        self.label = QLabel('Start')
+        button = QPushButton('DANGER')
+        button.pressed.connect(self.oh_no)
+
+        layout.addWidget(self.label)
+        layout.addWidget(button)
+
+        centralWidget = QWidget()
+        centralWidget.setLayout(layout)
+        self.setCentralWidget(centralWidget)
+
+        self.show()
+
+        self.timer = QTimer()
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(self.recurring_timer)
+        self.timer.start()
+
+    def oh_no(self):
+        time.sleep(5)
+
+    def recurring_timer(self):
+        self.counter += 1
+        self.label.setText(f'Counter: {self.counter}')
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
