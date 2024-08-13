002a16174dc56a5f77b841b34d95eff7fee4a927
eike-zink <user@eike-zink.de>
2024-08-13 20:57:15 +0200

    Teil 2, Filtering data

--- DataVisualizationTool/app.py
+++ DataVisualizationTool/app.py
@@ -1,9 +1,27 @@
 import argparse
 import pandas as pd
 
+from PySide6.QtCore import  QDateTime, QTimeZone
 
+
+def transform_date(utc, timezone=None):
+    """Transform String to UTC-Date"""
+    utc_fmt = 'yyyy-MM-ddTHH:mm:ss.zzzZ'
+    new_date =QDateTime().fromString(utc, utc_fmt)
+    if timezone:
+        new_date.setTimeZone(timezone)
+    return new_date
 def read_date(file_name):
-    return pd.read_csv(file_name)
+    # Read the CSV content
+    df = pd.read_csv(file_name)
+    # Remove wrong magnitudes
+    df = df.drop(df[df.mag < 0].index)
+    magnitudes = df['mag']
+    # Set local timezone
+    timezone = QTimeZone(b'Europe/Berlin')
+    # Get timestamp transformed to our timezone
+    times = df['time'].apply(lambda x: transform_date(x, timezone))
+    return times, magnitudes
 
 
 if __name__ == '__main__':
